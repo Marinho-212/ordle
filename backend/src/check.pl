5510367d4b254edd8c2755cc49cf446b9b51c294
Davy Coelho <salomao_jesus1994@outlook.com>
2025-04-08 22:20:04 -0300

    casdcwac!c
    Co-Authored-By: Marinho-212 <184698792+Marinho-212@users.noreply.github.com>
    Co-Authored-By: Caua-Kelvin <140714560+Caua-Kelvin@users.noreply.github.com>
    Co-Authored-By: CeciliaLucena <140714456+CeciliaLucena@users.noreply.github.com>

--- backend/src/check.pl
+++ backend/src/check.pl
@@ -4,11 +4,18 @@ check_classico(IdTry, Dic):-
     check_classico_name(IdDay,IdTry,NameResult),
     check_classico_age(IdDay,IdTry, AgeResult),
     check_classico_association(IdDay,IdTry,AssociationResult),
-    check_classico_fstap(IdDay,IdTry,FstapResult),
-    check_classico_affinity(IdDay,IdTry,AffinityResult),
-    check_classico_gender(IdDay,IdTry,GenderResult),
-    Dic = [name: NameResult, age: AgeResult, association: AssociationResult, 
-           fstap: FstapResult, affinity: AffinityResult, gender: GenderResult]).
+    % check_classico_fstap(IdDay,IdTry,FstapResult),
+    % check_classico_actor(IdDay,IdTry,ActorResult),
+    % check_classico_affinity(IdDay,IdTry,AffinityResult),
+    % check_classico_gender(IdDay,IdTry,GenderResult),
+    Dic = _{name: NameResult, 
+        	age: AgeResult,
+            association: AssociationResult
+        %    fstap: FstapResult, 
+        %    actor: ActorResult, 
+        %    affinity: AffinityResult, 
+        %    gender: GenderResult
+           }).
 
 check_classico_name(IdDay,IdTry,Saida):-
     personagem(IdDay,NameDay,_,_,_,_,_,_,_,_,_,_),
@@ -29,9 +36,9 @@ check_classico_association(IdDay,IdTry, Saida):-
     split_string(associationTry, ", ", "", listaTry),
     sort(listaDay, sortedDay),
     sort(listaTry, sortedTry),
-    verifica_association(sortedDay,sortedTry,Saida).
+    verifica(sortedDay,sortedTry,Saida).
 
-verifica_association(List1, List2, Result):-
+verifica(List1, List2, Result):-
     (List1 = List2 -> Result = 1;
      (common_element(List1,List2)-> Result = 0;
       Result = -1)).
@@ -40,19 +47,20 @@ common_element([Head|_], List2) :- member(Head, List2), !.
 common_element([_|Tail], List2) :- common_element(Tail, List2).
 
 
-/*verifica_associationDay([headDay|tailDay],sortedTry,Saida):-
-    verifica_associationTry([headDay|tailDay],sortedTry,Saida),
-    verifica_associationDay(tailDay,sortedTry,Saida).
-
-verifica_associationTry([headDay|tailDay],[headTry|tailTry],Saida):-
-    headDay = headTry -> Saida = 0;
-    verifica_associationTry([headDay|tailDay],tailTry).*/
-
 check_classico_fstap(IdDay,IdTry,Saida):-
     personagem(IdDay,_,_,_,_,fstapDay,_,_,_,_,_,_),
     personagem(IdTry,_,_,_,_,fstapTry,_,_,_,_,_,_),
     fstapDay = fstapTry -> Saida = 1;
     Saida = -1.
+
+check_classico_actor(IdDay,IdTry, Saida):-
+    personagem(IdDay,_,_,_,_,_,actorDay,_,_,_,_,_),
+    personagem(IdTry,_,_,_,_,_,actorTry,_,_,_,_,_),
+    split_string(actorDay, ", ", "", listaDay),
+    split_string(actorTry, ", ", "", listaTry),
+    sort(listaDay, sortedDay),
+    sort(listaTry, sortedTry),
+    verifica(sortedDay,sortedTry,Saida).
 
 check_classico_affinity(IdDay,IdTry,Saida):-
     personagem(IdDay,_,_,_,_,_,_,AffinityDay,_,_,_,_),
@@ -66,17 +74,20 @@ check_classico_gender(IdDay,IdTry,Saida):-
     GenderDay = GenderTry -> Saida = 1;
     Saida = -1.
 
-check_monstro(IdTry,Saida):-
+check_monstro(IdTry,Saida,Dic):-
     daily_entity(IdDay,"monster"),
-    (IdDay = IdTry -> Saida  is 1;
-    Saida is -1).
+    (IdDay = IdTry -> Saida = 1;
+    Saida = -1),
+    Dic = _{monstro: Saida}.
 
-check_emojis(IdTry,Saida):-
+check_emojis(IdTry,Saida,Dic):-
     daily_entity(IdDay,"emojis"),
-    (IdDay = IdTry -> Saida  is 1;
-    Saida is -1).
+    (IdDay = IdTry -> Saida = 1;
+    Saida is -1),
+    Dic = _{emoji: Saida}.
 
-check_falas(IdTry,Saida):-
+check_falas(IdTry,Saida,Dic):-
     daily_entity(IdDay,"quote"),
-   ( IdDay = IdTry -> Saida  is 1;
-    Saida is -1).
+   (IdDay = IdTry -> Saida = 1;
+    Saida is -1),
+    Dic = _{fala: Saida}.
--- backend/src/main.pl
+++ backend/src/main.pl
@@ -1,7 +1,12 @@
 :- [carrega_dados].
 :- [set_daily].
+:- [check].
 :- use_module(json_to_facts, [personagem/12, monstro/3, carregar_json/4]).
 :- use_module(library(persistency)).
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_json)).
+:- use_module(library(http/http_parameters)).
 :- initialization(main).
 
 :- http_handler(root(get_classic), get_classic_handler, []).
@@ -9,8 +14,13 @@
 :- http_handler(root(get_emojis), get_emojis_handler, []).
 :- http_handler(root(get_monster), get_monster_handler, []).
 
-get_classic_handler(_Request) :-
-    reply_json_dict(_{message: "Hello, World!"}).
+
+get_classic_handler(Request) :-
+    http_parameters(Request, [id(ID, [integer])]),
+    check_classico(ID, Dic),
+    % json:json_write_dict(Stream, Dic, []),
+    reply_json(Dic).
+
 
 main :-
     absolute_file_name('fact.db', File, [access(write)]),
